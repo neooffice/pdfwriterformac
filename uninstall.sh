b89794b6e05b104dbdcf5bd35a9067c7164c51e5
PL <pluby@guibomacbookpro.local>
2022-06-06 11:03:16 -0400

    Simplify deletion of /Library/Printers/Lisanet folder

--- uninstall.sh
+++ uninstall.sh
@@ -6,14 +6,13 @@
 # Created by Simone Karin Lehmann on 30.08.10.
 # Copyright 2010 Simone Karin Lehmann. All rights reserved.
 
-sudo rm /Library/Printers/Lisanet/PDFwriter/*
-sudo rm /Library/Printers/Lisanet/PDFwriter/.DS_S*
-sudo rm /Library/Printers/Lisanet/.DS_S*
-sudo rm /usr/libexec/cups/backend/pdfwriter
-sudo rm /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
+sudo rm -f /usr/libexec/cups/backend/pdfwriter
+sudo rm -f /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
+sudo rm -Rf /Library/Printers/Lisanet
 
-
-sudo rmdir /Library/Printers/Lisanet/PDFwriter
-sudo rmdir /Library/Printers/Lisanet
-
+# forget package
 sudo pkgutil --forget de.lisanet.PDFwriter.pkg 
+
+# restart cupsd
+launchctl stop org.cups.cupsd
+launchctl start org.cups.cupsd
