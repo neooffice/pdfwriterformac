fd25da9e5a8cef1b58b5087ed2a3fbc84d0530af
PL <pluby@guibomacmini.local>
2022-06-07 16:45:07 -0400

    Move chown and chmod of the bundle to a script in the PDFwriter target. Also, skip putting the bundle in the archive and just leave the bundle in $(TARGET_DIR) so that in the future we can enhance the script to build a .pkg directory.

--- postinstall.sh
+++ postinstall.sh
@@ -6,14 +6,11 @@
 # Created by Simone Karin Lehmann on 30.08.10.
 # Copyright 2010 Simone Karin Lehmann. All rights reserved.
 
-# copy PPD and make it read by all so that the Add Printer dialog will find it
-rm -f /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
-cp -f /Library/Printers/Lisanet/pdfwriter.bundle/Contents/Resources/PDFwriter.ppd /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
-chown -f root:admin /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
-chmod -f 644 /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
 
 # make symlinks
+umask 022
 ln -sf /Library/Printers/Lisanet/pdfwriter.bundle/Contents/MacOS/pdfwriter /usr/libexec/cups/backend/pdfwriter
+ln -sf /Library/Printers/Lisanet/pdfwriter.bundle/Contents/Resources/PDFwriter.ppd /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
 ln -sf /var/spool/pdfwriter /Users/Shared/PDFwriter
 
 # restart cupsd
