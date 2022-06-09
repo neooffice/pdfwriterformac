74912a074bd48c47b32d72ecf84a451313c82442
PL <pluby@guibomacmini.local>
2022-06-08 20:31:58 -0400

    Implement building of the installer's Bom and Payload files. Also, move all of the linking and copying in the post install script to softlinks within the Bom and Payload files.

--- createarchivefilesforinstaller.sh
+++ createarchivefilesforinstaller.sh
@@ -11,20 +11,14 @@ function print_help_and_exit {
 		message="Error: $1"
 		echo "$message" >&2
 	fi
-	echo "Usage: '$0' <SOURCE_ROOT> <TARGET_BUILD_DIR> <EXECUTABLE_PATH> <UNLOCALIZED_RESOURCES_FOLDER_PATH> <CODESIGNING_FOLDER_PATH> <EXPANDED_CODE_SIGN_IDENTITY_NAME>" >&2
+	echo "Usage: '$0' <TARGET_BUILD_DIR> <ARCHIVE_BUILD_DIR> <USER> <GROUP>" >&2
 	exit 1
 }
 
 # Parse arguments
-if [ $# -lt 8 ] ; then
+if [ $# -lt 4 ] ; then
 	print_help_and_exit
 fi
-
-if [ -z "$1" -o ! -d "$1" ] ; then
-	print_help_and_exit "The SOURCE_ROOT argument '$1' is not set or is set to a non-existant directory"
-fi
-export SOURCE_ROOT="$1"
-shift
 
 if [ -z "$1" -o ! -d "$1" ] ; then
 	print_help_and_exit "The TARGET_BUILD_DIR argument '$1' is not set or is set to a non-existant directory"
@@ -32,28 +26,10 @@ fi
 export TARGET_BUILD_DIR="$1"
 shift
 
-if [ -z "$TARGET_BUILD_DIR/$1" -o ! -f "$TARGET_BUILD_DIR/$1" ] ; then
-	print_help_and_exit "The EXECUTABLE_PATH argument '$1' is not set or is set to a non-existant file"
-fi
-export EXECUTABLE_PATH="$1"
-shift
-
-if [ -z "$TARGET_BUILD_DIR/$1" -o ! -d "$TARGET_BUILD_DIR/$1" ] ; then
-	print_help_and_exit "The UNLOCALIZED_RESOURCES_FOLDER_PATH argument '$1' is not set or is set to a non-existant directory"
-fi
-export UNLOCALIZED_RESOURCES_FOLDER_PATH="$1"
-shift
-
 if [ -z "$1" -o ! -d "$1" ] ; then
-	print_help_and_exit "The CODESIGNING_FOLDER_PATH argument '$1' is not set or is set to a non-existant directory"
+	print_help_and_exit "The ARCHIVE_BUILD_DIR argument '$1' is not set or is set to a non-existant directory"
 fi
-export CODESIGNING_FOLDER_PATH="$1"
-shift
-
-if [ -z "$1" ] ; then
-	print_help_and_exit "The EXPANDED_CODE_SIGN_IDENTITY_NAME argument '$1' is not set"
-fi
-export EXPANDED_CODE_SIGN_IDENTITY_NAME="$1"
+export ARCHIVE_BUILD_DIR="$1"
 shift
 
 if [ -z "$1" ] ; then
@@ -78,38 +54,19 @@ fi
 # Set a non-restrictive umask
 umask 022
 
-# Create build directories
-codesigning_bundle_dir=`dirname "$CODESIGNING_FOLDER_PATH"`
-codesigning_bundle_name=`basename "$CODESIGNING_FOLDER_PATH"`
-renamed_bundle_name=PDFwriter
-archive_build_dir="$TARGET_BUILD_DIR/$codesigning_bundle_name.archive"
-rm -Rf "$archive_build_dir"
+# Chown the bundle
+chown -R root:admin "$ARCHIVE_BUILD_DIR"
+chown root:wheel "$ARCHIVE_BUILD_DIR"
+chown root:wheel "$ARCHIVE_BUILD_DIR/Library"
+chown root:wheel "$ARCHIVE_BUILD_DIR/Users/Shared"
+chown -R root:wheel "$ARCHIVE_BUILD_DIR/usr"
 
-lisanet_archive_build_dir="$archive_build_dir/Library/Printers/Lisanet"
-ppd_archive_build_dir="$ppd_archive_build_dir/Library/Printers/PPDs/Contents/Resources"
-shared_user_archive_build_dir="$archive_build_dir/Users/Shared"
-mkdir -p "$lisanet_archive_build_dir"
-mkdir -p "$ppd_archive_build_dir"
-mkdir -p "$shared_user_archive_build_dir"
+# Create Bom and Payload files
+rm -f "$TARGET_BUILD_DIR/Bom" "$TARGET_BUILD_DIR/Payload"
+( cd "$ARCHIVE_BUILD_DIR" && mkbom . "$TARGET_BUILD_DIR/Bom" )
+( cd "$ARCHIVE_BUILD_DIR" && pax -w -z -x cpio . > "$TARGET_BUILD_DIR/Payload" )
 
-# Copy the bundle
-( cd "$codesigning_bundle_dir" && tar cf - "$codesigning_bundle_name" ) | ( cd "$lisanet_archive_build_dir" && tar xvf - )
-mv "$lisanet_archive_build_dir/$UNLOCALIZED_RESOURCES_FOLDER_PATH/PDFwriter.ppd" "$ppd_archive_build_dir/PDFwriter.ppd"
-ln -s "/var/spool/pdfwriter" "$shared_user_archive_build_dir/PDFwriter"
-
-# Set permissions required by cups
-find "$archive_build_dir" -type d -exec chmod 755 {} \;
-find "$archive_build_dir" -type f -exec chmod 644 {} \;
-find "$archive_build_dir" -type l -exec chmod -h 644 {} \;
-chmod 700 "$lisanet_archive_build_dir/$EXECUTABLE_PATH"
-chmod 750 "$lisanet_archive_build_dir/$UNLOCALIZED_RESOURCES_FOLDER_PATH/uninstall.sh"
-
-# Rename, codesign, and chown the copied bundle
-new_bundle_name=PDFwriter
-mv "$lisanet_archive_build_dir/$codesigning_bundle_name" "$lisanet_archive_build_dir/$renamed_bundle_name"
-( cd "$lisanet_archive_build_dir" && sudo -u "$USER" codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY_NAME" "$renamed_bundle_name" )
-chown -R root:admin "$lisanet_archive_build_dir"
-chown root:wheel "$lisanet_archive_build_dir/Library"
-chown root:wheel "$lisanet_archive_build_dir/Users/Shared"
+# Chown the bundle, Bom and Payload files to Xcode user
+chown -R "$USER:$GROUP" "$ARCHIVE_BUILD_DIR" "$TARGET_BUILD_DIR/Bom" "$TARGET_BUILD_DIR/Payload"
 
 exit 0
--- postinstall.sh
+++ postinstall.sh
@@ -6,13 +6,6 @@
 # Created by Simone Karin Lehmann on 30.08.10.
 # Copyright 2010 Simone Karin Lehmann. All rights reserved.
 
-
-# make symlinks
-umask 022
-ln -sf /Library/Printers/Lisanet/pdfwriter.bundle/Contents/MacOS/pdfwriter /usr/libexec/cups/backend/pdfwriter
-ln -sf /Library/Printers/Lisanet/pdfwriter.bundle/Contents/Resources/PDFwriter.ppd /Library/Printers/PPDs/Contents/Resources/PDFwriter.ppd
-ln -sf /var/spool/pdfwriter /Users/Shared/PDFwriter
-
 # restart cupsd
 launchctl unload /System/Library/LaunchDaemons/org.cups.cupsd.plist
 launchctl load /System/Library/LaunchDaemons/org.cups.cupsd.plist
